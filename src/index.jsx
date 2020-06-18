139cd6cb67070598d3084b4c2eccde2c73b3a3fa
mairasala <mairasala@gmail.com>
2020-06-18 11:59:46 +0200

    include missing files

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { ThemeProvider } from '@material-ui/core/styles' 
-
 // import{ baseTheme } from 'ava-theme-creator'
-console.log(ThemeProvider)
+
 const MyTheme = ({children}) => (
-  <ThemeProvider theme={{}}>
-    <div>
-      <div>{children}</div>
-      <button>hello!</button>
-    </div>
-  </ThemeProvider>
+  <div>
+    <div>{children}</div>
+    <button>hello!</button>
+  </div>
 )
 
 MyTheme.propTypes = {
